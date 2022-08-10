38deae3abaa71322426047806459f3433e8457f4
Akash2377 <surveakash01@gmail.com>
2022-08-11 00:33:31 +0530

    server added

--- db.js
+++ db.js
@@ -3,11 +3,12 @@ const tags = require("./data/tags");
 const brands = require("./data/brands");
 const itemTypes = require("./data/itemTypes");
 const zee5 = require("./data/zee5");
-
+const loginDetails = require("./data/userloginDetails");
 module.exports = {
   items,
   tags,
   brands,
   itemTypes,
   zee5,
+  loginDetails,
 };
