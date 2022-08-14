3f34933e0e1ed9a57585778aceba47b413b62190
Akash2377 <surveakash01@gmail.com>
2022-08-14 16:26:10 +0530

    Masai2.0 on server

--- db.js
+++ db.js
@@ -7,6 +7,10 @@ const userloginDetails = require("./data/userloginDetails");
 const zeeOriginalData = require("./data/zeeOriginalData");
 const TvShowsZee5 = require("./data/TvShowsZee5");
 const webseries = require("./data/webseries");
+const studentLoginData = require("./data/studentLoginData");
+const superAdminLoginData = require("./data/superAdminLoginData");
+const adminLoginData = require("./data/adminLoginData");
+const courses = require("./data/courses");
 module.exports = {
   items,
   tags,
@@ -17,4 +21,8 @@ module.exports = {
   zeeOriginalData,
   TvShowsZee5,
   webseries,
+  courses,
+  adminLoginData,
+  superAdminLoginData,
+  studentLoginData,
 };
