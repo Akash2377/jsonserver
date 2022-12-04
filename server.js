30f79fb9a0a62d6cc9c85a4b4a7542cd2a2ec0d7
Akash Surve <surveakash01@gmail.com>
2022-12-04 19:07:58 +0530

    cors added

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const data = require("./db.js");
-
+const cors = require("cors");
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router(data);
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
-
+server.use(cors());
 server.use(middlewares);
 server.use(router);
 
